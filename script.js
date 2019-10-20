c87e4ff38838b539f78596a08a5303acc1c18267
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-10-20 13:15:04 -0400

    added homecoming

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ const input = document.querySelector(".input")
 const url3 = "https://api.lyrics.ovh/v1/beyonce/"
 form.addEventListener("submit", function (evt) {
     evt.preventDefault()
-    console.log(input.value)
-    console.log(url3 + input.value)
+    // console.log(input.value)
+    // console.log(url3 + input.value)
     fetch(url3 + input.value)
         .then(res => res.json())
         .then(res => {
@@ -30,12 +30,7 @@ form.addEventListener("submit", function (evt) {
         .catch(err => console.log(err))
  })    // this site has lyrics but you can't grab for the whole artist
 
-    // fetch(url3)
-    // .then (res => res.json())
-    // .then (res => console.log(res))
-    // .catch (err => console.log (err))
-
-const url4 = "https://api.giphy.com/v1/gifs/random?api_key=5VHD8krqyXl6NCnL26GYoLw8yxwc9NH8&tag=beyonce&rating=G"
+const url4 = "https://api.giphy.com/v1/gifs/random?api_key=5VHD8krqyXl6NCnL26GYoLw8yxwc9NH8&tag=beychella&rating=G"
 const randomButton = document.querySelector(".randomButton")
 randomButton.addEventListener("click", function(evt){
     evt.preventDefault()
