6140050bf600edd06911793203cbb637e37dc348
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-10-20 01:51:15 -0400

    added the lyrics

--- script.js
+++ script.js
@@ -12,12 +12,28 @@ const url2 = "https://itunes.apple.com/search?term=beyonce"
     .then (res => console.log(res))
     .catch (err => console.log (err))
 
-// this site has lyrics but you can't grab for the whole artist
-const url3 = "https://api.lyrics.ovh/v1/beyonce/1+1/"
-    fetch(url3)
-    .then (res => res.json())
-    .then (res => console.log(res))
-    .catch (err => console.log (err))
+const lyrics = document.querySelector(".lyrics")
+const form = document.querySelector('form')
+const searchButton = document.querySelector(".searchButton")
+const input = document.querySelector(".input")
+const url3 = "https://api.lyrics.ovh/v1/beyonce/"
+form.addEventListener("submit", function (evt) {
+    evt.preventDefault()
+    console.log(input.value)
+    console.log(url3 + input.value)
+    fetch(url3 + input.value)
+        .then(res => res.json())
+        .then(res => {
+            console.log(res)
+            lyrics.innerText = res.lyrics
+        })
+        .catch(err => console.log(err))
+ })    // this site has lyrics but you can't grab for the whole artist
+
+    // fetch(url3)
+    // .then (res => res.json())
+    // .then (res => console.log(res))
+    // .catch (err => console.log (err))
 
 const url4 = "https://api.giphy.com/v1/gifs/random?api_key=5VHD8krqyXl6NCnL26GYoLw8yxwc9NH8&tag=beyonce&rating=G"
 const randomButton = document.querySelector(".randomButton")
